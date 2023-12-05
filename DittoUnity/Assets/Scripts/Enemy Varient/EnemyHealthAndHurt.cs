e198343fb57cba67a0a3363173f2767d605f6a74
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2023-12-05 09:42:18 -0800

    HEALTH

--- DittoUnity/Assets/Scripts/Enemy Varient/EnemyHealthAndHurt.cs
+++ DittoUnity/Assets/Scripts/Enemy Varient/EnemyHealthAndHurt.cs
@@ -4,41 +4,52 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthAndHurt : MonoBehaviour
 {
     public float maxHealth;
     public float health;
     private Animator _animator;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Camera camera;
 
     public void Start()
     {
         health = maxHealth;
         _animator = GetComponent<Animator>();
+        camera = 
+    }
+
+    public void FixedUpdate()
+    {
+        slider.transform.rotation = camera.transform.rotation;
     }
 
     public void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("playerBullet"))
         {
-            health--;
-            _animator.Play("HurtAnim");
-            if (health <= 0)
-            {
-                Destroy(gameObject);
-            }
+            Damage(1f);
         }
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("playerBullet"))
         {
-            health--;
-            _animator.Play("HurtAnim");
-            if (health <= 0)
-            {
-                Destroy(gameObject);
-            }
+            Damage(1f);
         }
+    }
+
+    public void Damage(float amount)
+    {
+        health-=amount;
+        _animator.Play("HurtAnim");
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        slider.value = health / maxHealth;
     }
 }
