18360c04c8750b493d1d455c2d279dc5f7ebba7d
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2023-12-07 09:09:25 -0800

    5

--- DittoUnity/Assets/Scripts/DoorOpen.cs
+++ DittoUnity/Assets/Scripts/DoorOpen.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 public class DoorOpen : MonoBehaviour
 {
     public bool open;
-	private static int levelNum = 1;
+	private static int levelNum = 0;
     private SpriteRenderer sr;
 
     [SerializeField] Sprite openSprite;
@@ -33,7 +33,15 @@ public class DoorOpen : MonoBehaviour
         if (open && other.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.W))
         {
             levelNum++;
-            SceneManager.LoadScene("Level" + levelNum);
+            try
+            {
+                SceneManager.LoadScene("Level" + levelNum);
+            }
+            catch (Exception e)
+            {
+                
+            }
+            
         }
     }
 }
